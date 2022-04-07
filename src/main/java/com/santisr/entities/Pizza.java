044978384b65efc698e82f8ec0b6a20b03d10dfb
SantiagoSR <santi72alc@gmail.com>
2022-04-07 13:43:21 +0200

    Creando las primeras entities

---
+++ src/main/java/com/santisr/entities/Pizza.java
@@ -0,0 +1,10 @@
+package com.santisr.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pizza implements Serializable {
+    
+}
--- src/main/java/com/santisr/main/PizzeriaApplication.java
+++ src/main/java/com/santisr/main/PizzeriaApplication.java
@@ -1,4 +1,4 @@
-package com.santisr.pizzeria;
+package com.santisr.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
